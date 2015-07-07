c28795dde6ed81d4abeb8cf9bf94779713e69948
cjwddtc <leeshengyuan@live.com>
2015-07-07 23:09:13 +0800

    start

---
+++ web/Login.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+public class UserLogin : UserControl
+{
+    protected HtmlTable LoginTable;
+    protected TextBox UserName;
+    protected TextBox Password;
+
+    public string BackColor
+    {
+        get { return LoginTable.BgColor; }
+        set { LoginTable.BgColor = value; }
+    }
+ 
+    public string Name
+    {
+        get { return UserName.Text; }
+        set { UserName.Text = value; }
+    }
+ 
+    public string Pas
+    {
+        get { return Password.Text; }
+        set { Password.Text = value; }
+    }
+
+    public delegate void LoginEventHandler (Object sender,
+        String e);
+
+    public event LoginEventHandler Login;
+
+    public void OnLoginButtonClicked (Object sender, EventArgs e)
+    {
+        if (Login != null) {
+            Driver a = new datacontrol("D:\\asd.data").getDriver(Name);
+
+            if (a != null && a.password == Pas)
+            {
+                Session["driver"] = a;
+                Login(this, a.password);
+            }
+            else Login(this, null);
+        }
+    }
+}
---
+++ web/LoginPage.aspx
@@ -0,0 +1,31 @@
+<%@ Register TagPrefix="user" TagName="LoginControl" src="LoginControl.ascx" %>
+<%@ Page Language="C#" Debug="true" %>
+
+<html>
+  <body>
+    <h1>Drivers login</h1>
+    <hr>
+    <form runat="server">
+      <user:LoginControl ID="Login" OnLogin="OnLoginUser" BackColor="#ccccff" RunAt="server" />
+    </form>
+    <hr>
+    <h3><asp:Label ID="Output" RunAt="server" /></h3>
+  </body>
+</html>
+
+<script language="C#" runat="server">
+  void OnLoginUser (Object sender, String e)
+  {
+      if (e != null)
+      {
+          Driver a = (Driver)Session["driver"];
+          Output.Text = "Hello, " + a.password;
+      }
+      else
+      {
+          Output.Text = "the id or password you input wrong!";
+          Login.Name = "";
+          Login.Pas = "";
+      }
+  }
+</script>
